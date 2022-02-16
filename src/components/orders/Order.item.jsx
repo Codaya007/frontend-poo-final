7cacaecd464a15d4e9b63353ea768625ab4fab43
Viviana <vivicalvat007@gmail.com>
2022-02-16 15:52:25 -0500

    Eliminando importacion innecesaria

--- src/components/orders/Order.item.jsx
+++ src/components/orders/Order.item.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { loadOrderDetail } from "../../data/actions";
 import { useNavigate } from "react-router-dom";
 
 const OrderItem = ({ order }) => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
   const { createdAt, products, status, totalAmount } = order;
 
   const handleClickDetail = () => {
-    dispatch(loadOrderDetail(order));
     navigate(`/order/${order._id}`);
   };
 
