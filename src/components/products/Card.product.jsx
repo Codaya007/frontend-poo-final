e26eae263b1162537371c860cb3a5237a5eb65d5
Viviana <vivicalvat007@gmail.com>
2022-02-18 12:01:33 -0500

    Añadiendo pagina de ProductDetail

--- src/components/products/Card.product.jsx
+++ src/components/products/Card.product.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, delFromCart } from "../../data/actions";
+import { Link } from "react-router-dom";
 
 const CardProduct = ({ product }) => {
-  const { name, photo, price, sold, _id } = product;
+  const { name, photo, price, description, _id } = product;
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.products.cart);
   const [inCart, setInCart] = useState(cart.find((e) => e._id === _id));
@@ -22,27 +23,41 @@ const CardProduct = ({ product }) => {
     <div className="col-md-3" key={product._id}>
       <div className="card mt-4 mb-4 ms-3 me-3 rounded-15">
         <div className="card-header">
-          <h3>{product.name}</h3>
-          <span className="badge rounded-15 bg-success">{product.price}</span>
+          <Link style={{ textDecoration: "none" }} to={`/product/${_id}`}>
+            <h3>{name}</h3>
+          </Link>
+          <span className="badge rounded-15 bg-success">{price}</span>
         </div>
         <div className="card-body">
-          <img className="img-fluid imagen" src={product.photo} alt="" />
+          <img className="img-fluid imagen" src={photo} alt="" />
           <div className="dropend b-grid">
-            <button className="btn btn-info dropdown-toggle" type="button" id={"dropdownMenu" + product._id} data-bs-toggle="dropdown" aria-expanded="false">
+            <button
+              className="btn btn-info dropdown-toggle"
+              type="button"
+              id={"dropdownMenu" + _id}
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
               Descripcion
             </button>
-            <ul className="dropdown-menu bg-secondary" aria-labelledby={"dropdownMenu" + product._id}>
-              <p className="ps-2 pe-2 text-light text-justify">{product.description}</p>
+            <ul
+              className="dropdown-menu bg-secondary"
+              aria-labelledby={"dropdownMenu" + _id}
+            >
+              <p className="ps-2 pe-2 text-light text-justify">{description}</p>
             </ul>
           </div>
         </div>
         <div className="card-footer">
-          <button className="btn btn-primary" onClick={inCart ? handleRemoveFromCart : handleAddCart}>
+          <button
+            className="btn btn-primary"
+            onClick={inCart ? handleRemoveFromCart : handleAddCart}
+          >
             {inCart ? "Quitar del carrito" : "Añadir al carrito"}
           </button>
         </div>
       </div>
-    </div >
+    </div>
 
     // <div class="col">
     //   <div className="card">
@@ -57,7 +72,6 @@ const CardProduct = ({ product }) => {
     //     </button>
     //   </div>
     // </div>
-
   );
 };
 
--- src/screens/ProductDetail.jsx
+++ src/screens/ProductDetail.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { BASEURL } from "../assets/constants";
+import Loader from "../components/loader/Loader";
+import { addToCart } from "../data/actions";
 
-const Productdetail = ({ product }) => {
-  const {
-    name = "Bicicleta",
-    description = "Muy bonita xd",
-    price = 250,
-    category = "Nose",
-    quantity = 0,
-    photo,
-  } = product;
-  return (
+const Productdetail = () => {
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const { name, description, price, category, quantity, photo } = product || {};
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const { data } = await axios.get(`${BASEURL}/product/${id}`);
+        setProduct({ ...data });
+      } catch (err) {
+        toast.error("No se ha obtener la información del producto");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getProduct();
+  }, [id]);
+
+  return loading ? (
+    <Loader />
+  ) : product ? (
     <div>
       <h2>{name}</h2>
       <img src={photo} alt={name} />
@@ -29,7 +50,14 @@ const Productdetail = ({ product }) => {
         <h3>Cantidad</h3>
         <p>{quantity}</p>
       </div>
-      {quantity > 0 && <button>Añadir al carrito</button>}
+      {quantity > 0 && (
+        <button onClick={dispatch(addToCart(id))}>Añadir al carrito</button>
+      )}
+    </div>
+  ) : (
+    <div>
+      <h3>404</h3>
+      <div>Producto no encontrado</div>
     </div>
   );
 };
