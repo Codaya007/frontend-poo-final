3a942495ed6eb9b97a7331082710af78fd46c378
AlekZ11 <yerik.espinoza@unl.edu.ec>
2022-02-21 13:40:23 -0500

    stilizing filter

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   filterByCategory,
@@ -6,12 +6,31 @@ import {
   setOptions,
 } from "../../data/actions";
 import { RESTART_FILTERS, RESTART_PRODUCTS } from "../../data/actions/types";
+import { toast } from "react-toastify";
+import { searchByName } from "../../data/actions";
 
 const Filters = () => {
   const categories = useSelector((state) => state.products.categories);
   // options -> order, orderBy
   const options = useSelector((state) => state.products.options);
   const dispatch = useDispatch();
+  const [search, setSearch] = useState("");
+
+  const handleChangeSearch = (e) => {
+    const { value } = e.target;
+
+    setSearch(value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    if (!search) {
+      toast.info("Ingrese su búsqueda");
+    } else {
+      dispatch(searchByName(search));
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -37,52 +56,67 @@ const Filters = () => {
   };
 
   return (
-    <div>
-      <label className="">Ordenar por</label>
-      <select
-        title="orderBy"
-        className=""
-        name={"orderBy"}
-        id={"orderBy"}
-        onChange={handleChange}
-        value={options.orderBy}
-      >
-        <option value={"name"} key={"name"}>
-          {"Nombre"}
-        </option>
-        <option value={"price"} key={"price"}>
-          {"Precio"}
-        </option>
-      </select>
-      <select
-        title="order"
-        name={"order"}
-        id={"order"}
-        onChange={handleChange}
-        value={options.order}
-      >
-        <option value={"asc"} key={"asc"}>
-          {"Ascendente"}
-        </option>
-        <option value={"desc"} key={"desc"}>
-          {"Descendente"}
-        </option>
-      </select>
-      <label>Categorías</label>
-      <select
-        className="form-select"
-        name="category"
-        onChange={handleChange}
-        value={options.category}
-      >
-        <option value={"all"}>Todas</option>
-        {categories.map((e) => (
-          <option key={e._id} value={e._id}>
-            {e.name}
+    <div className="mt-3 container d-flex justify-content-center">
+      <form className="d-flex" onSubmit={handleSearch}>
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          onChange={handleChangeSearch}
+        />
+        <button className="btn btn-success text-login" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="d-flex align-items-center ms-3">
+        <label className="text-light">Ordenar:</label>
+        <select
+          title="orderBy"
+          className="rounded p-1 ms-2 me-2"
+          name={"orderBy"}
+          id={"orderBy"}
+          onChange={handleChange}
+          value={options.orderBy}
+        >
+          <option value={"name"} key={"name"}>
+            {"Nombre"}
           </option>
-        ))}
-      </select>
-      <button onClick={handleRestartFilters}>Quitar filtros</button>
+          <option value={"price"} key={"price"}>
+            {"Precio"}
+          </option>
+        </select>
+        <select
+          title="order"
+          name={"order"}
+          id={"order"}
+          className="rounded p-1 ms-2 me-2"
+          onChange={handleChange}
+          value={options.order}
+        >
+          <option value={"asc"} key={"asc"}>
+            {"Ascendente"}
+          </option>
+          <option value={"desc"} key={"desc"}>
+            {"Descendente"}
+          </option>
+        </select>
+        <label className="text-light">Categorías:</label>
+        <select
+          className="rounded p-1 ms-2 me-2"
+          name="category"
+          onChange={handleChange}
+          value={options.category}
+        >
+          <option value={"all"}>Todas</option>
+          {categories.map((e) => (
+            <option key={e._id} value={e._id}>
+              {e.name}
+            </option>
+          ))}
+        </select>
+        <button onClick={handleRestartFilters} className="btn btn-secondary">Quitar filtros</button>
+      </div>
     </div>
   );
 };
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,51 +3,17 @@ import { useSelector, useDispatch } from "react-redux";
 import CardProduct from "../components/products/Card.product";
 import { FaShoppingCart } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import { searchByName } from "../data/actions";
 import Filters from "../components/filters/Filters";
 
 const Home = () => {
   const products = useSelector((state) => state.products.filtered);
   const cart = useSelector((state) => state.products.cart);
-  const [search, setSearch] = useState("");
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const handleChangeSearch = (e) => {
-    const { value } = e.target;
-
-    setSearch(value);
-  };
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-
-    if (!search) {
-      toast.info("Ingrese su búsqueda");
-    } else {
-      dispatch(searchByName(search));
-    }
-  };
 
   // console.log(products);
   return (
     <div className="container containerToReduce">
       <h1 className="text-light fw-bold fst-italic">Home</h1>
-      <div className="text-end mt-2">
-        <form className="d-flex ms-3" onSubmit={handleSearch}>
-          <input
-            className="form-control me-2 text-login"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-            onChange={handleChangeSearch}
-          />
-          <button className="btn btn-success text-login" type="submit">
-            Search
-          </button>
-        </form>
-      </div>
       <div>
         <Filters />
       </div>
